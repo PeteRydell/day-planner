5c051ccbe34f5d3d3b5d379ad344e12213d74639
Peter Rydell <peterydell@gmail.com>
2019-10-19 16:53:50 -0700

    code cleanup

--- script.js
+++ script.js
@@ -30,55 +30,47 @@ $("#descriptionFive").val(fivePM);
 //Sets value of description elements on save button click and saves to local storage
 $("#saveBtnNine").click(function() { 
     var value = $('#descriptionNine').val(); 
-    console.log(value)
     localStorage.setItem('#descriptionNine', JSON.stringify(value));
+    console.log(value)
 }); 
 
 $("#saveBtnTen").click(function() { 
     var value = $('#descriptionTen').val(); 
-    console.log(value)
     localStorage.setItem('#descriptionTen', JSON.stringify(value));
 }); 
 
 $("#saveBtnEleven").click(function() { 
     var value = $('#descriptionEleven').val(); 
-    console.log(value)
     localStorage.setItem('#descriptionEleven', JSON.stringify(value));
 }); 
 
 $("#saveBtnTwelve").click(function() { 
     var value = $('#descriptionTwelve').val(); 
-    console.log(value)
     localStorage.setItem('#descriptionTwelve', JSON.stringify(value));
 }); 
 
 $("#saveBtnOne").click(function() { 
     var value = $('#descriptionOne').val(); 
-    console.log(value)
     localStorage.setItem('#descriptionOne', JSON.stringify(value));
 }); 
 
 $("#saveBtnTwo").click(function() { 
     var value = $('#descriptionTwo').val(); 
-    console.log(value)
     localStorage.setItem('#descriptionTwo', JSON.stringify(value));
 }); 
 
 $("#saveBtnThree").click(function() { 
     var value = $('#descriptionThree').val(); 
-    console.log(value)
     localStorage.setItem('#descriptionThree', JSON.stringify(value));
 }); 
 
 $("#saveBtnFour").click(function() { 
     var value = $('#descriptionFour').val(); 
-    console.log(value)
     localStorage.setItem('#descriptionFour', JSON.stringify(value));
 }); 
 
 $("#saveBtnFive").click(function() { 
     var value = $('#descriptionFive').val(); 
-    console.log(value)
     localStorage.setItem('#descriptionFive', JSON.stringify(value));
 }); 
 
