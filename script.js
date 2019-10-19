7dee19872bbc48e03548a7120bc23a59a653aca7
Peter Rydell <peterydell@gmail.com>
2019-10-19 14:56:11 -0700

    local storage for time blocks

--- script.js
+++ script.js
@@ -1,25 +1,76 @@
 $("#currentDay").text(moment().format('LLLL'));
 
-var nineAM = JSON.parse(localStorage.getItem('#descriptionNine'));
-var tenAM = JSON.parse(localStorage.getItem('tenAM'));
-var elevenAM = JSON.parse(localStorage.getItem('elevenAM'));
-var twelvePM = JSON.parse(localStorage.getItem('twelvePM'));
-var onePM = JSON.parse(localStorage.getItem('onePM'));
-var twoPM = JSON.parse(localStorage.getItem('twoPM'));
-var threePM = JSON.parse(localStorage.getItem('threePM'));
-var fourPM = JSON.parse(localStorage.getItem('fourPM'));
-var fivePM = JSON.parse(localStorage.getItem('fivePM'));
+var time = 'HH:mm:ss'
 
-var nineAm = "";
+var nineAM = JSON.parse(localStorage.getItem('#descriptionNine'));
+$("#descriptionNine").val(nineAM);
+var tenAM = JSON.parse(localStorage.getItem('#descriptionTen'));
+$("#descriptionTen").val(tenAM);
+var elevenAM = JSON.parse(localStorage.getItem('#descriptionEleven'));
+$("#descriptionEleven").val(elevenAM);
+var twelvePM = JSON.parse(localStorage.getItem('#descriptionTwelve'));
+$("#descriptionTwelve").val(twelvePM);
+var onePM = JSON.parse(localStorage.getItem('#descriptionOne'));
+$("#descriptionOne").val(onePM);
+var twoPM = JSON.parse(localStorage.getItem('#descriptionTwo'));
+$("#descriptionTwo").val(twoPM);
+var threePM = JSON.parse(localStorage.getItem('#descriptionThree'));
+$("#descriptionThree").val(threePM);
+var fourPM = JSON.parse(localStorage.getItem('#descriptionFour'));
+$("#descriptionFour").val(fourPM);
+var fivePM = JSON.parse(localStorage.getItem('#descriptionFive'));
+$("#descriptionFive").val(fivePM);
 
 $("#saveBtnNine").click(function() { 
-    var nineAm = $('#descriptionNine').val(nineAm); 
-    console.log(nineAm)
+    var value = $('#descriptionNine').val(); 
+    console.log(value)
+    localStorage.setItem('#descriptionNine', JSON.stringify(value));
 }); 
 
+$("#saveBtnTen").click(function() { 
+    var value = $('#descriptionTen').val(); 
+    console.log(value)
+    localStorage.setItem('#descriptionTen', JSON.stringify(value));
+}); 
 
+$("#saveBtnEleven").click(function() { 
+    var value = $('#descriptionEleven').val(); 
+    console.log(value)
+    localStorage.setItem('#descriptionEleven', JSON.stringify(value));
+}); 
 
+$("#saveBtnTwelve").click(function() { 
+    var value = $('#descriptionTwelve').val(); 
+    console.log(value)
+    localStorage.setItem('#descriptionTwelve', JSON.stringify(value));
+}); 
 
+$("#saveBtnOne").click(function() { 
+    var value = $('#descriptionOne').val(); 
+    console.log(value)
+    localStorage.setItem('#descriptionOne', JSON.stringify(value));
+}); 
 
+$("#saveBtnTwo").click(function() { 
+    var value = $('#descriptionTwo').val(); 
+    console.log(value)
+    localStorage.setItem('#descriptionTwo', JSON.stringify(value));
+}); 
 
+$("#saveBtnThree").click(function() { 
+    var value = $('#descriptionThree').val(); 
+    console.log(value)
+    localStorage.setItem('#descriptionThree', JSON.stringify(value));
+}); 
 
+$("#saveBtnFour").click(function() { 
+    var value = $('#descriptionFour').val(); 
+    console.log(value)
+    localStorage.setItem('#descriptionFour', JSON.stringify(value));
+}); 
+
+$("#saveBtnFive").click(function() { 
+    var value = $('#descriptionFive').val(); 
+    console.log(value)
+    localStorage.setItem('#descriptionFive', JSON.stringify(value));
+}); 
